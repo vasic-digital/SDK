0e9ddcde43fa8e2bfcb06399a9e153ec7bd1248c
Milos Vasic <i@mvasic.ru>
2024-08-11 19:57:15 +0300

    Upstreams.

---
+++ Upstreams/GitFlic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@gitflic.ru:boba/boba-for-android-sdk.git"
---
+++ Upstreams/Gitee.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@gitee.com:milosvasic/boba-for-android-sdk.git"
