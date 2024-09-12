189b80c3560d1c258fac3d4eec3939aa0a8cf19a
Milos Vasic <i@mvasic.ru>
2024-09-12 15:39:48 +0300

    Small cleanup.

--- Android/SDK/src/main/java/net/boba/sdk/common/IPCService.kt
+++
@@ -1,9 +0,0 @@
-package net.boba.sdk.common
-
-import net.boba.sdk.common.ipc.IPCRequest
-import net.boba.sdk.common.ipc.IPCResponse
-
-interface IPCService {
-
-    fun onRequest(request: IPCRequest): IPCResponse
-}
--- Android/SDK/src/main/java/net/boba/sdk/common/application/IPCApplication.kt
+++ Android/SDK/src/main/java/net/boba/sdk/common/application/IPCApplication.kt
@@ -4,9 +4,8 @@ import com.redelf.commons.extensions.obfuscate
 import com.redelf.commons.interprocess.InterprocessApplication
 import com.redelf.commons.interprocess.InterprocessProcessor
 import net.boba.R
-import net.boba.sdk.common.IPCService
 
-abstract class IPCApplication : InterprocessApplication(), IPCService {
+abstract class IPCApplication : InterprocessApplication() {
 
     override val firebaseEnabled = false
     override val interprocessPermission = R.string.interprocess_permission
