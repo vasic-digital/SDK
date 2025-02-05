3c123d05fcbe392684ab3c96a40cba3bbc00b040
Milos Vasic <i@mvasic.ru>
2025-02-05 18:45:49 +0300

    Auto-commit.

--- Android/SDK/src/main/java/net/boba/sdk/common/ipc/IPCExchange.kt
+++ Android/SDK/src/main/java/net/boba/sdk/common/ipc/IPCExchange.kt
@@ -1,7 +1,6 @@
 package net.boba.sdk.common.ipc
 
 import com.redelf.commons.interprocess.InterprocessData
-import com.redelf.commons.interprocess.InterprocessError
 import java.util.UUID
 
 interface IPCExchange {
--- Android/SDK/src/main/java/net/boba/sdk/common/ipc/IPCFailure.kt
+++ Android/SDK/src/main/java/net/boba/sdk/common/ipc/IPCFailure.kt
@@ -1,8 +1,6 @@
 package net.boba.sdk.common.ipc
 
-import com.redelf.commons.interprocess.InterprocessData
 import com.redelf.commons.interprocess.InterprocessError
-import java.util.UUID
 
 interface IPCFailure {
 
