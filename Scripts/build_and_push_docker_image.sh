f87b4b88b7abfb928f9cafcd1e69a7c9a24ea247
Milos Vasic <i@mvasic.ru>
2025-03-02 18:13:29 +0300

    Small cleanup.

--- Scripts/build_and_push_docker_image.sh
+++
@@ -1,3 +0,0 @@
-docker build -t flow-app-proxy ./proxy
-docker tag flow-app-proxy registry.digitalocean.com/flow-app/flow-app-proxy
-docker push registry.digitalocean.com/flow-app/flow-app-proxy
--- Scripts/distribute_Production.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./gradlew assembleProductionRelease appDistributionUploadProductionRelease
-
-#firebase appdistribution:distribute --app "1:655267848300:android:1ed59e1d29690ad60c7849" --groups "Dev" \
-#  --release-notes "The production test build" Application/build/outputs/apk/production/release/Application-production-release.apk
